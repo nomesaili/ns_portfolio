bdf9939f5a9e028e973e6dbdc8f8a1c09545759f
nomesaili <nome.saili@gmail.com>
2022-12-22 10:06:28 +1300

    Working mroing 22 dec 2022

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Blog #Import blog entry model into views
 # Create your views here.
 def all_blogs(request):
 
-    blogs = Blog.objects.all() #instantiate project data into variable projects
+    blogs = Blog.objects.order_by('-date')[:5] #instantiate project data into variable projects
 
-    return render(request, 'blog/all_blogs.html', {'blog':blogs})
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
 
     
      
